3f7355859deea2e0d5ad603353daf5a61aa005c8
Nichita Pasecinic <nichittaa@gmail.com>
2023-04-02 14:54:00 +0300

    fix(docker): config

--- gateway/config/prod.exs
+++ gateway/config/prod.exs
@@ -3,7 +3,7 @@ import Config
 # General configs
 config :gateway,
   auth_service_base_url: "http://auth_service",
-  main_api_service_base_url: "http://main_api:8081",
+  main_api_service_base_url: "http://main_api",
   db_api_service_base_url: "http://connector-service"
 
 # Do not print debug messages in production
